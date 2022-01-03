8e97df20f05f8f54a0e8c4f9b25b8def3ce5830e
josephlongo123 <josepheugenelongo@gmail.com>
2022-01-02 22:35:50 -0500

    Problem Seven

---
+++ ProblemSeven.py
@@ -0,0 +1,13 @@
+from ProblemThree import primeChecker
+import ProblemThree
+
+#To generate a list of prime numbers, we can reuse the function from Problem Three
+#If that function is too slow to compute 10,001 primes, we can try to write a faster prime function
+
+primes = []
+n = 2
+while len(primes) < 10001:
+    if ProblemThree.primeChecker(n):
+        primes.append(n)
+    n+=1
+print(primes[-1])
